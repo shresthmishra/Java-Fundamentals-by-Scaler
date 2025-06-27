d6d9fbbce1d8b56672393468cfe4b02e8795b72b
Shresth Mishra <shresthmishr@gmail.com>
2025-06-27 20:25:49 +0530

    For loop - Example code done

---
+++ Loops/ForLoop.java
@@ -0,0 +1,9 @@
+// Print numbers from 1 to 10
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
