83063438f205b654f2200ea44e5a64b0f71aace9
Shresth Mishra <shresthmishr@gmail.com>
2025-06-28 02:24:45 +0530

    Arrays

---
+++ Arrays/Array.java
@@ -0,0 +1,14 @@
+// Array is a data structure which helps in storing data of the same type in a sequential manner
+// An Array takes a contigous section of the memory
+
+public class Array {
+    public static void main(String[] args) {
+        int[] integerarray = {1,2,3,4,5,6,7};
+        String[] stringarray = {"Hello", "there", "good", "people"};
+        float[] floatarray = {7, (float) 1.5};
+
+        System.out.println(integerarray[6]);
+        System.out.println(stringarray[0]);
+        System.out.println(floatarray[1]);
+    }    
+}
