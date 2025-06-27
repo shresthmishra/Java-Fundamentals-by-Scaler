f2b385cf4c05f852664fb6e32f37e62854d8dc86
Shresth Mishra <shresthmishr@gmail.com>
2025-06-27 21:50:24 +0530

    Do While - Example Code

---
+++ Loops/DoWhile.java
@@ -0,0 +1,13 @@
+// Print numbers till 7
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+        int number = 0;
+        do {
+            System.out.println(number);
+            number++;
+        } 
+        while (number != 8);
+    }
+}
