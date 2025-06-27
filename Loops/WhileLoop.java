9e99adda291dcf9038f73ad7885746596f7908ba
Shresth Mishra <shresthmishr@gmail.com>
2025-06-27 21:46:56 +0530

    While Loop - Example Code

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,17 @@
+// Count digits of a number
+
+import java.util.*;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int result = 0;
+        while (number != 0) {
+            number = number / 10;
+            result++;
+        }
+        System.out.println("The digit of the number is: " + result);
+    }
+}
