fc840bc98cbc6d5386ea167a94477cbef8afda82
Shresth Mishra <shresthmishr@gmail.com>
2025-07-04 23:10:56 +0530

    Code Foreach Loop example

---
+++ Loops/ForeachLoop.java
@@ -0,0 +1,10 @@
+// Printing numbers from 7 to 1
+
+public class ForeachLoop {
+    public static void main(String[] args) {
+        int[] arr = {7,6,5,4,3,2,1};
+        for (int num  : arr) {
+            System.out.println(num);
+        }
+    }
+}
